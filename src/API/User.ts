73852aeed099fed6ca2695f48bfbf0dcdca8d1b9
Ivan Busarov <Spaider629@gmail.com>
2019-06-24 16:37:56 +0300

    change api url

--- src/API/User.ts
+++ src/API/User.ts
@@ -1,6 +1,6 @@
 import { IUser } from "../state";
 
-const API_URL = 'http://api.backendless.com/53FB6874-BEE0-9546-FFCA-1F3DEE56BE00/73E38E38-C5C9-45B2-FFB5-D05A6E16A600/data/Users';
+const API_URL = 'https://api.backendless.com/53FB6874-BEE0-9546-FFCA-1F3DEE56BE00/73E38E38-C5C9-45B2-FFB5-D05A6E16A600/data/Users';
 
 export default class User {
     static makeRequest(url: string, method: string = 'GET', body?: string, ) {
